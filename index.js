b9417779720dda691184b600c77642bfdfc42c4e
RGGOLIK <152860170+RggolikSN@users.noreply.github.com>
2025-10-28 14:19:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function modrinth() {
+        document.location.href = 'https://rggoliksn.github.io/ModrinthModpacks/'
+}
+
+function rggoliksn() {
+        document.location.href = 'https://github.com/RggolikSN'
+}
